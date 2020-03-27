8135083d42cc0e5f024b0a74e60ab816db658abf
Tomas Costa <tomas.costa@localiza.com>
2020-03-27 17:16:40 -0300

    commit[

--- src/app/http.service.spec.ts
+++ src/app/http.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpService } from './http.service';
+import { HttpService } from './Services/http.service';
 
 describe('HttpService', () => {
   let service: HttpService;
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service'
+import { HttpService } from '../Services/http.service'
 
 @Component({
   selector: 'app-list',
